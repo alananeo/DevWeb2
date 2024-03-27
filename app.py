18f6366a824fb85c40a0b74bdd0e17b7980e060e
Alana <viana.alana@gmail.com>
2024-03-26 23:47:27 -0400

    Primeiro

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+with st.form("user_input_form"):
+    name = st.text_input("Nome")
+    email = st.text_input("Email")
+    submit_button = st.form_submit_button("Enviar")
+    if submit_button:
+        st.write(f"Nome: {name}")
+        st.write(f"Email: {email}")
+
+st.sidebar.text_input("LOGIN")
